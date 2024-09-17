7648ff0ad1be608429cdb4e39cf1e23fc4339c06
Akashi-01 <akashigupta15@gmail.com>
2024-09-18 01:23:38 +0530

    Create Education.jsx

---
+++ src/Components/Education.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import React from "react";
+
+const Education = () => {
+    return(
+        <section className="light" id="education">
+            <h2 style={{ textAlign: "center" }}>Education</h2>
+            <p style={{color: "grey"}}>B.tech in Computer Science and Engineering</p>
+            <p style={{color:"slateblue"}}>At Rajshree Institute of Management and Tachnology (Bareilly)</p>
+        </section>
+    )
+}
+
+export default Education;
